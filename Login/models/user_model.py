912205340cbc8f294ead765429a87a11016d1f09
GaloViturco <viturco12@gmail.com>
2025-07-11 22:13:21 -0500

    Change

--- Login/models/user_model.py
+++ Login/models/user_model.py
@@ -5,13 +5,13 @@ class UserModel:
     def find_user(email, password=None):
         conn, cursor = get_connection()
         if password:
-            cursor.execute("SELECT id FROM users WHERE email=? AND password=?", (email, password))
+            cursor.execute("SELECT id FROM user WHERE email=? AND password=?", (email, password))
         else:
-            cursor.execute("SELECT id FROM users WHERE email=?", (email,))
+            cursor.execute("SELECT id FROM user WHERE email=?", (email,))
         return cursor.fetchone()
 
     @staticmethod
     def create_user(email, password):
         conn, cursor = get_connection()
-        cursor.execute("INSERT INTO users (email, password) VALUES (?, ?)", (email, password))
+        cursor.execute("INSERT INTO user (email, password) VALUES (?, ?)", (email, password))
         conn.commit()
