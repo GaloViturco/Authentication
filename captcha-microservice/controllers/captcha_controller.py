70c410b2a03bb559745810753d82e2ab8594d345
Loony213 <kevinhdhd@hotmail.com>
2025-07-12 11:28:44 -0500

    feat: :sparkles: new micro

--- captcha-microservice/controllers/captcha_controller.py
+++ captcha-microservice/controllers/captcha_controller.py
@@ -1,11 +1,12 @@
 from flask import Flask, send_file, jsonify
-from flask_cors import CORS 
+from flask_cors import CORS
 from services.captcha_service import CaptchaService
 import io
+from PIL import Image
 
 app = Flask(__name__)
 
-CORS(app)
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.route('/generate-captcha', methods=['GET'])
 def generate_captcha():
@@ -13,9 +14,11 @@ def generate_captcha():
     captcha_text, captcha_image = CaptchaService.generate_captcha()
     
     img_io = io.BytesIO()
-    captcha_image.write(captcha_text, img_io)
-    img_io.seek(0)
-    
+
+    captcha_image = Image.open(captcha_image)  
+    captcha_image.save(img_io, 'PNG')  
+    img_io.seek(0)  
+
     return send_file(img_io, mimetype='image/png', as_attachment=True, download_name='captcha.png')
 
 if __name__ == '__main__':
